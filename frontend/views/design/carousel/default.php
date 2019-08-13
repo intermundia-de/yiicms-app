703cf72572bafc175bfd89116122f609a59f2b29
sleemy <levani19972@gmail.com>
2019-08-13 18:06:09 +0400

    Adjust control and bullet appearance

--- frontend/views/design/carousel/default.php
+++ frontend/views/design/carousel/default.php
@@ -10,26 +10,32 @@
 /** @var $contentTreeItem \frontend\models\ContentTree */
 /** @var $index integer */
 /** @var $model \intermundia\yiicms\models\Carousel */
+$directChildren = $contentTreeItem->getItemsQuery()->all();
 
 ?>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
+    <?php if (count($directChildren) > 0): ?>
+        <ol class="carousel-indicators">
+            <?php foreach ($directChildren as $key => $child): ?>
+                <li data-target="#carouselExampleIndicators" data-slide-to="<?php echo $key; ?>"
+                    class="<?php $key > 0 ? '' : 'active'; ?>"></li>
+            <?php endforeach; ?>
+        </ol>
+    <?php endif; ?>
     <div class="carousel-inner">
         <?php echo $this->render('@frontend/views/content-tree/list', [
             'viewFile' => null,
             'contentTreeItem' => $contentTreeItem
         ]); ?>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+    <?php if (count($directChildren) > 0): ?>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    <?php endif; ?>
 </div>
